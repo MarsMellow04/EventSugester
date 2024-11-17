6bf6ffa8be10f8a6bb5c1268a4a366c08bb50894
MarsMellow04 <louis.cutteridge@gmail.com>
2024-11-17 19:24:46 +0000

    Added Tag component that allows toggling

--- my-vite-app/src/components/DataFetchingComponent.tsx
+++ my-vite-app/src/components/DataFetchingComponent.tsx
@@ -1,26 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import ToggleButton from './TagComponent';
 
 interface Tag {
     id: number;
     name: string;
-  }
+    
+}
 
-  const DataFetchingComponent: React.FC = () => {
-  const [data, setData] = useState<Tag[]>([]);
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/GetSystemTags')
-      .then(response => response.json())
-      .then(json => setData(json));
-  }, []);
-  return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-6">
-      {data.map(item => (
-        <button key={item.id} className="default-button">
-          <h2 className="text-gray-700 font-semibold text-m mb-2" >{item.name}</h2>
-          {/* <p className="text-gray-700 text-base">{item.body}</p> */}
-        </button>
-      ))}
+function DataFetchingComponent() {
+    // Defining list of tags from query
+    const [toggledList, setList] = useState(new Array);
+    
+    const [data, setData] = useState<Tag[]>([]);
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/GetSystemTags')
+            .then(response => response.json())
+            .then(json => setData(json));
+    }, []);
+
+    function addToToggleList(tag: Tag){
+        //Mkae a item added to the list of toggled items
+        const isTag = (element) => element == tag;
+        // const indexToRemove = toggledList.findIndex((x) => x === tag)
+
+        if (toggledList?.includes(tag)) {
+            toggledList.splice(toggledList.findIndex(isTag), 1)
+            setList(toggledList)
+        } else {
+            toggledList.push(tag)
+            setList(toggledList)
+        }
+        toggledList.forEach((item, index) => {
+            console.log(`This is the current toggled items index: ${index}:${item.name}`)
+        })
+
+    }
+
+    return (
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-5">
+        {data.map(item => (
+            <ToggleButton key={item.id} value={item.name} onToggle={() => addToToggleList(item)}/>
+        ))}
     </div>
-  );
-};
-export default DataFetchingComponent;
+    );
+} export default DataFetchingComponent;
---
+++ my-vite-app/src/components/TagComponent.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function ToggleButton({value, onToggle}){    
+    const classes = {
+        'toggled': 'apperance-none bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full',
+        'untoggled': 'apperance-none bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+    }
+    const [isToggled, setToggle] = useState(false);
+    function handleClick() {
+        setToggle(!isToggled);
+        onToggle();
+        console.log(`this classes isToggled is: ${isToggled}`)
+    }
+    
+    return (
+        < button onClick={handleClick} className={isToggled? `${classes['toggled']}`: `${classes['untoggled']}`}>
+            {value}
+        </button>
+    );
+} export default ToggleButton;
