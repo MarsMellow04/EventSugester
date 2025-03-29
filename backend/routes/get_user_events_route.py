8e650c9706b813fb42ea856986bd58c2761828d1
MarsMellow04 <louis.cutteridge@gmail.com>
2025-03-29 12:41:28 +0000

    Assitance with debugging

--- backend/routes/get_user_events_route.py
+++ backend/routes/get_user_events_route.py
@@ -1,10 +1,13 @@
 from flask import request, abort
 from database import db, Event
 from util import get_user_from_context
+import sys
 
 def get_user_events():
   user = get_user_from_context()
   if not user: abort(403)
 
   user_events = db.session.query(Event).filter_by(organiser_email=user.email).all()
+  # print(user.email, file=sys.stderr)
+  print(user_events[0].get_dict().items(), file=sys.stderr)
   return [event.get_dict() for event in user_events]
